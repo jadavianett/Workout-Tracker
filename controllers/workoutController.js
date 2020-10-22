cf75d0c52a70e8521f213c6ac427d7f6540673e1
Jada Arnett <jada.arnett@gmail.com>
2020-10-21 23:28:18 -0400

    add routes to add workouts and add exercises and display on homepage

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -1,12 +1,67 @@
+const express = require("express");
 const router = express.Router();
 const db = require("../models");
-const express = require("express");
+
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then((foundWorkout) => {
+        res.json(foundWorkout);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: true,
+          message: "Did not create new workout.",
+        });
+      });
+  });
+
+  router.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
+      .then((newWorkout) => {
+        res.json(newWorkout);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: true,
+          message: "Did not create new workout.",
+        });
+      });
+  });
 
 
+  router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+      req.params.id,
+      { $push: { exercises: req.body } },
+      { new: true }
+    )
+      .then((newExercise) => {
+        res.json(newExercise);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: true,
+          message: "Did not update workout.",
+        });
+      });
+  });
 
-
+  router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .limit(5)
+      .then((foundWorkout) => {
+        res.json(foundWorkout);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: true,
+          message: "Did not update workout.",
+        });
+      });
+  });
 
 module.exports = router;
-
-
-
