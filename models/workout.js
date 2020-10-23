b153042234017d2a7470baf031fd3caebafac635
Jada Arnett <jada.arnett@gmail.com>
2020-10-22 20:11:06 -0400

    adding a virtual to the model

--- models/workout.js
+++ models/workout.js
@@ -1,38 +1,50 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now(),
-  },
-
-  exercises: [
-    {
-      type: {
-        type: String,
-      },
-      name: {
-        type: String,
-      },
-      distance: {
-        type: Number,
-      },
-      duration: {
-        type: Number,
-      },
-      weight: {
-        type: Number,
-      },
-      sets: {
-        type: Number,
-      },
-      reps: {
-        type: Number,
-      },
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now(),
     },
-  ],
-});
 
+    exercises: [
+      {
+        type: {
+          type: String,
+        },
+        name: {
+          type: String,
+        },
+        distance: {
+          type: Number,
+        },
+        duration: {
+          type: Number,
+        },
+        weight: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+return this.exercises.reduce((total, exercise)=> {
+return total + exercise.duration
+},0)
+});
 const Workout = mongoose.model("Workout", WorkoutSchema);
 module.exports = Workout;
