e68998f8d8283f60c6dec808e5204d2ded88d142
Jada Arnett <jada.arnett@gmail.com>
2020-10-22 20:44:22 -0400

    adding comments to code

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,13 @@
+
 const path = require("path");
 const {dirname} = require("path");
 module.exports = function(app){
 
+    // returns the stats page
     app.get("/stats", (req,res) => {
         res.sendFile(path.join(__dirname, "../public/stats.html"))
     });
-    
+    // returns the exercise page 
     app.get("/exercise", (req,res) => {
         res.sendFile(path.join(__dirname, "../public/exercise.html"))
     });
